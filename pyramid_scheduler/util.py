a80844b69d71fa3f0643c4ed4fd0861ea26ac03e
grabner <pjg.github@ubergrabner.net>
2013-08-19 16:06:01 -0400

    added "resolve()" helper method (to lookup dotted module/attribute names)

--- pyramid_scheduler/util.py
+++ pyramid_scheduler/util.py
@@ -99,6 +99,25 @@ def cull(**kw):
   return {key : value for key, value in kw.items() if value is not None}
 
 #------------------------------------------------------------------------------
+def resolve(spec):
+  if not spec or callable(spec):
+    return spec
+  if ':' in spec:
+    spec, attr = spec.split(':', 1)
+    return getattr(resolve(spec), attr)
+  spec = spec.split('.')
+  used = spec.pop(0)
+  found = __import__(used)
+  for cur in spec:
+    used += '.' + cur
+    try:
+      found = getattr(found, cur)
+    except AttributeError:
+      __import__(used)
+      found = getattr(found, cur)
+  return found
+
+#------------------------------------------------------------------------------
 def now():
   return time.time()
 
